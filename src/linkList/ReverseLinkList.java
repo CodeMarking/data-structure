c38ba5bbe303925b0351514e1a5543dd2cf0e870
make <codemarking@gmail.com>
2022-06-09 23:43:04 +0800

    区间链表翻转

--- src/linkList/ReverseLinkList.java
+++ src/linkList/ReverseLinkList.java
@@ -45,6 +45,48 @@ public class ReverseLinkList {
 
 
     /**
+     * 翻转链表的处理
+     * @param head 头节点
+     * @param m  起始位置。
+     * @param n  结束位置。
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || m >= n) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        head = dummy;
+        for (int i = 1; i < m; i++) {
+            head = head.next;
+        }
+        ListNode prevM = head;
+        ListNode mNode = head.next;
+        ListNode nNode = mNode;
+        ListNode postN = nNode.next;
+        // nNode => prev;
+        //postN => current
+        for(int i = m; i < n; i++) {
+            /**
+             ListNode next = current.next;
+             current.next = prev;
+             prev = current;
+             current = next;
+             **/
+            ListNode next = postN.next;
+            postN.next = nNode;
+            nNode = postN;
+            postN = next;
+        }
+        mNode.next = postN;
+        prevM.next = nNode;
+        return dummy.next;
+    }
+
+
+
+    /**
      * definition  linkListNode
      */
 
